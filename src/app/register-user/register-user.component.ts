8c5520c19d44a57dc57f6ee627705eb690e05fb6
Gloify <neha@gloify.com>
2018-09-20 10:31:31 +0530

    first commit

---
+++ src/app/register-user/register-user.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-user',
+  templateUrl: './register-user.component.html',
+  styleUrls: ['./register-user.component.css']
+})
+export class RegisterUserComponent implements OnInit {
+
+let name="Vivek";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
